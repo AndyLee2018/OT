3a2c8a34d1c85bc67e2f8823fda16dac9c00e65b
AndyLee2018 <taihangxingren@163.com>
2021-04-28 16:28:34 +0800

    整合日志框架

--- src/main/java/com/example/provider/api/impl/UserServiceImpl.java
+++ src/main/java/com/example/provider/api/impl/UserServiceImpl.java
@@ -4,6 +4,8 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.example.provider.api.UserService;
 import com.example.provider.dao.UserinfoMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -21,9 +23,9 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserinfoMapper userinfoMapper;
 
-    public static Integer num = 0;
-
     public static AtomicLong increase = new AtomicLong(0);
+
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Autowired
     public void label(RedisTemplate<String,String> redisTemplate){
@@ -39,8 +41,9 @@ public class UserServiceImpl implements UserService {
         while (i < increase.intValue()){
             list.add(new String());
             i++;
+            log.info("自增编号：{}",i++);
         }
-        System.out.println(increase);
+        log.info("编号：{}",increase);
         return JSON.toJSONString(increase);
     }
 
